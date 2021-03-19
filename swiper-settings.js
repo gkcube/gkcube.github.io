adfbffed0ab0e1b604f147ef3cf6416e7404c49f
gkcube <73008567+gkcube@users.noreply.github.com>
2021-03-20 00:43:31 +0530

    Update swiper-settings.js

--- swiper-settings.js
+++ swiper-settings.js
@@ -2,19 +2,13 @@ $(document).ready(function() {var swiper = new Swiper('.swiper-container',{loop:
                               
 navigation: {nextEl: '.next-right',prevEl: '.next-left',},       });
                                 
-								
-var n1 = localStorage.getItem('n1');
-      				
 swiper.on('transitionEnd', function() {$(".next-right").removeClass("mynext");
-localStorage.setItem('n1', swiper.activeIndex);  }); 
-                         
+localStorage.setItem('n1', swiper.activeIndex);  });
+var n1 = localStorage.getItem('n1');
+                                
 function nbox() {if (localStorage.getItem("n1") === null) {
 swiper.slideTo(1,0,true); }else {swiper.slideTo(n1,0,true);}  }
 
-$('.reset').click(function() {$("#popup").fadeToggle('fast');  });
-
-nbox();
-
+$('.reset').click(function() { rhash();});
     
-    
-  });
+    nbox();  });
